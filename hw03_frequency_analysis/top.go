84833c4fec0dfeacf7de9e0c10fa9f304ca6dcc7
SShlykov <zeitment@gmail.com>
2024-02-15 16:46:31 +0300

    feat: solved freq analysis

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,38 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"strings"
+	"unicode"
+)
+
+// Top10 возвращает 10 самых часто встречающихся слов в порядке убывания частоты.
+// Если слова с одинаковой частотой, то сортируются лексикографически.
+func Top10(text string) []string {
+	wordSet := makeWordSet(text)
+	wordHeap := NewWordHeap()
+	for word, freq := range wordSet {
+		wordHeap.Add(word, freq)
+	}
+
+	return wordHeap.TopN(10)
+}
+
+// makeWordSet создает не стандартное множество слов из текста (вместо bool хранит частоту слова)
+func makeWordSet(text string) map[string]int {
+	wordSet := make(map[string]int)
+
+	words := strings.FieldsFunc(text, func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c) && c != '-'
+	})
+
+	for _, word := range words {
+		word = strings.ToLower(word)
+
+		if word == "" || word == "-" {
+			continue
+		}
+		wordSet[word]++
+	}
+
+	return wordSet
 }
