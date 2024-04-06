cac6ccc3d085639577efe226dcc55fe36c3a3a43
SShlykov <zeitment@gmail.com>
2024-04-06 22:44:35 +0300

    feat: removed outlist

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,52 +9,33 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	currentIn := in
+
+	for _, stage := range stages {
+		currentIn = stage(currentIn)
+	}
+
 	out := make(Bi)
-	finish := make(chan struct{})
-	outs := prepareOuts(in, stages...)
-	go RunOuts(outs, done, finish)
 
 	go func() {
-	handler:
+		defer close(out)
+
 		for {
 			select {
-			case res, ok := <-outs[len(outs)-1]:
-				if !ok {
-					close(finish)
-					break handler
-				}
-				out <- res
 			case <-done:
-				break handler
+				return
+			case res, ok := <-currentIn:
+				if !ok {
+					return
+				}
+				select {
+				case out <- res:
+				case <-done:
+					return
+				}
 			}
 		}
-		close(out)
 	}()
 
 	return out
-}
-
-func prepareOuts(in In, stages ...Stage) []Out {
-	outs := make([]Out, len(stages))
-	for i, stage := range stages {
-		outs[i] = stage(in)
-		in = outs[i]
-	}
-	return outs
-}
-
-func RunOuts(outs []Out, done In, finish chan struct{}) {
-	select {
-	case <-done:
-		for _, currOut := range outs {
-			go readUntilClosed(currOut)
-		}
-	case <-finish:
-		return
-	}
-}
-
-func readUntilClosed(out Out) {
-	for range out {
-	}
 }
